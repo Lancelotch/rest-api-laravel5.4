4fe97f4196520bc9677ec9fd0b3dc6b0ddf75cf6
Candra Darmawan <chandra.souvenir@gmail.com>
2018-09-24 10:12:48 +0800

    seeding for Product and review

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,24 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+// $factory->define(App\Model\User::class, function (Faker $faker) {
+//     return [
+//         'name' => $faker->word,
+//         'detail' => $faker->paragraph,
+//         'price' => $faker->numberBetween(100,1000),
+//         'stock' => $faker->randomDigit,
+//         'discount'=>$faker->numberBetween(2,30)
+//     ];
+// });
+//
+// $factory->define(App\Model\User::class, function (Faker $faker) {
+//     return [
+//         'product_id' => function(){
+//           return Product::all()->random();
+//         },
+//         'cusomer' => $faker->name,
+//         'review' => $faker->paragraph,
+//         'star' => $faker->numberBetween(0,5)
+//     ];
+// });
