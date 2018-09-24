e29c2accd028e5fef8181d463c4c2fc957073640
Candra Darmawan <chandra.souvenir@gmail.com>
2018-09-24 10:24:37 +0800

    Create Relationship table products and reviews

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Reviews;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+      return $this->hasMany(Review::class);
+    }
 }
